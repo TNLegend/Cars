51406bc907dc4593bbe1f8e807781965a0bb2e58
RIB <yacineabich123@gmail.com>
2023-01-14 12:35:16 +0100

    cars

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {Box} from "@mui/material";
 
+// importing components
+import Right from "./components/Right";
+import Left from "./components/Left";
+import "./App.css"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="app">
+      <Left/>
+      <Right/>
+    </Box>
   );
 }
 
